310d322f6acc194f2fdfac908e2333428e634968
ibrabet <147070384+ibrabet@users.noreply.github.com>
2024-03-08 20:23:47 +0300

    Turn it into a five-round game

--- script.js
+++ script.js
@@ -1,50 +1,63 @@
+let playerScore = 0;
+let computerScore = 0;
+let gameResultsMessage = '';
+
+function playGame() {
+    for (let i = 0; i <= 4; i++) {
+        console.log('');
+        const playerSelection = getPlayerSelection();
+        const computerSelection = getComputerChoice();
+        console.log(`Round: ${i + 1}`);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if (playerScore === computerScore) {
+        gameResultsMessage = `It's a draw!`;
+    } else if (playerScore > computerScore) {
+        gameResultsMessage = `You won the game!`;
+    } else {
+        gameResultsMessage = `You lost the game!`;
+    }
+    console.log('');
+    console.log(`Results:\nYour score: ${playerScore}\nComputer score: ${computerScore}\n${gameResultsMessage}`);
+}
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return `It's a tie! ${playerSelection} and ${computerSelection}!`;
-    } 
-    else if ((playerSelection === 'Rock' && computerSelection === 'Scissors')
+    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors')
         || (playerSelection === 'Paper' && computerSelection === 'Rock')
         || (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
-        return `You win! ${playerSelection} beats ${computerSelection}!`;
-    } 
-    else {
+            playerScore++;
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
+        
+    } else {
+        computerScore++;
         return `You lose! ${computerSelection} beats ${playerSelection}!`;
     }
 }
 
-
-const playerSelection = getPlayerSelection();
-const computerSelection = getComputerChoice();
-
-
-//  function for computer's choice
-//  randomly returns 'Rock', 'Paper', or 'Scissors' 
 function getComputerChoice() {
     const randomInt = Math.floor(Math.random() * 3);
     const selectionsArr = ['Rock', 'Paper', 'Scissors'];
     return selectionsArr[randomInt];
-
 }
 
-// function for player's choice
-// asks player to enter the choice
-// takes its first letter in upper case
-// if it is 'R' then playerSelection is 'Rock'
-// if it is 'P' then playerSelection is 'Paper'
-// otherwise playerSelection is 'Scissors'
 function getPlayerSelection() {
-    let playerSelection;
-    playerSelection = prompt('Choose Rock, Paper or Scissors: ');
-    const firstLetter = playerSelection[0].toUpperCase();
-    if (firstLetter === 'R') {
-        playerSelection = 'Rock';
-    } else if (firstLetter === 'P') {
-        playerSelection = 'Paper';
+    let playerSelection = prompt(`Choose: Rock, Paper or Scissors\nor click 'OK' for random choice: `, '');
+    if (playerSelection === '') {
+        playerSelection = getComputerChoice();
     } else {
-        playerSelection = 'Scissors';
+        const firstLetter = playerSelection[0].toUpperCase();
+        if (firstLetter === 'R') {
+            playerSelection = 'Rock';
+        } else if (firstLetter === 'P') {
+            playerSelection = 'Paper';
+        } else {
+            playerSelection = 'Scissors';
+        }
     }
     return playerSelection;
 }
 
-// console.log(playRound(playerSelection, computerSelection));
-alert(playRound(playerSelection, computerSelection));
+playGame();
